a55b06afbbbd14d439fd9acd912450ab27b64ae0
Elen Tenekejyan <elen.tenekejyan00@gmail.com>
2024-08-02 02:16:30 +0400

    modal test commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => setOpen(true)} className="open-modal-btn">
-        ✨ Open Window
-      </button>
-
       {open && (
         <div className="overlay">
           <div className="modal">
